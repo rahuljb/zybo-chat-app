985e829782724b1cb4240daaa6502dfb31b107c9
Rahul <jbrahul.jr@gmail.com>
2026-03-01 05:03:26 +0530

    Modify:asgi file changes added

--- config/asgi.py
+++ config/asgi.py
@@ -1,17 +1,19 @@
-# yourproject/asgi.py
+# config/asgi.py
 
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
 
-# Tell Django which settings to use when ASGI loads
+# 1️⃣ Tell Django which settings to use BEFORE importing anything that uses models
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-# Create the Django ASGI application early so that it can be used by both
-# HTTP and WebSocket.
+# 2️⃣ Create the base Django ASGI app
 django_asgi_app = get_asgi_application()
+
+# 3️⃣ Now it is safe to import chat.routing (it will import models etc.)
+from chat.routing import websocket_urlpatterns
+
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
