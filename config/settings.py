d2c6d96ee60a31cec78255845d9e4d34bb81b240
Rahul <jbrahul.jr@gmail.com>
2026-03-01 05:22:20 +0530

    Modify:settings file csrf changes added

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,9 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://zybo-chat-app.onrender.com",
+]
 INSTALLED_APPS = [
     'daphne',              
     'django.contrib.admin',
