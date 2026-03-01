c4709d29c8fb5f1282d230edfee5e570a98fe6ed
Rahul <jbrahul.jr@gmail.com>
2026-03-01 05:31:55 +0530

    Fix DATABASES: remove sslmode for SQLite on Render

--- config/settings.py
+++ config/settings.py
@@ -66,9 +66,9 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default="sqlite:///db.sqlite3",
         conn_max_age=600,
-        ssl_require=bool(os.environ.get("RENDER")),
+        
     )
 }
 
